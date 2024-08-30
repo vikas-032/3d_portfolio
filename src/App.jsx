7675777acc91a986c258a196285daee0b1daa401
Vikas Kumar <7014.vikas@gmail.com>
2024-08-30 15:12:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>    <h1 className="text-3xl font-bold underline text-red-500">
-    Hello world!
+    Hello world
+      thus is new update
   </h1>
   </div>
 
