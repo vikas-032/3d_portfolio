2e3eb728acb42c585a8e66ecb08dc2841a6a647f
Vikas <7014.vikas@example.com>
2024-08-30 14:49:19 +0530

    problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>    <h1 className="text-3xl font-bold underline">
+    <div>    <h1 className="text-3xl font-bold underline text-red-500">
     Hello world!
   </h1>
   </div>
