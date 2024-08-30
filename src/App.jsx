07552fd251da378a93037c6b54b4949dd2570c23
Vikas <7014.vikas@gmail.com>
2024-08-30 15:29:40 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>    <h1 className="text-3xl font-bold underline text-red-500">
-<<<<<<< HEAD
     Hello world!
     
-=======
-    Hello world
-      thus is new update
->>>>>>> 7675777acc91a986c258a196285daee0b1daa401
   </h1>
   </div>
 
