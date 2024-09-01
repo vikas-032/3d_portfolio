48bd0eec2841e8c90cea6955122e4a94e5ceb007
Vikas <7014.vikas@gmail.com>
2024-09-01 23:55:06 +0530

    error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -19,6 +20,9 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight /> 
+
+
+          <Island />
         </Suspense>
       </Canvas>
     </section>
